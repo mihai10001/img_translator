7c05a3948b2a6ee917b037c2d2b56bd9b0ca318e
mihai10001 <rvivits@gmail.com>
2020-02-18 11:57:36 +0200

    Revert "added flask and file-upload logic"
    This reverts commit 33923c5f2f95a4db6c2acfc4c0117647ebde6deb.

--- app/options_pipeline.py
+++ app/options_pipeline.py
@@ -1,5 +1,5 @@
 from language.language_detect import detect_language
-from language.spell_check import spell_check
+from language.spell_check import spell_checker
 from language.string_ops import clean
 
 
@@ -8,7 +8,5 @@ def pipeline(string, user_options):
 
     if user_options.get('detect_lang'):
         lang, prob = detect_language(string)
-
     if user_options.get('spell_checker'):
-        if lang:
-            corrected_string = spell_check(string)
+        corrected_string = spell_checker(string)
