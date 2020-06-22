3444b1d288decb1bc93216003ac078216e25fb76
mihai10001 <rvivits@gmail.com>
2020-06-22 16:20:43 +0300

    create analyse endpoint, call detection/translation API wrappers

--- app/main.py
+++ app/main.py
@@ -90,22 +90,52 @@ def customize(filename):
         return render_template('customize.html', error='Image not uploaded!')
 
 
-@app.route('/analysed/<filename>/<user_option>', methods=['GET', 'POST'])
-def analysed(filename, user_option):
+@app.route('/analyse/<filename>/<detection_method>/<user_option>', methods=['GET', 'POST'])
+def analyse(filename, detection_method, user_option):
     user_option = json.loads(user_option)
+    to_language = user_option.get('translate_to')
 
     if filename:
-        image = load_image(os.path.join(UPLOAD_FOLDER, 'images', filename))
-        random_strings = ["Buna sera, doamna", "Oare ce facem aici?", "Unde e aici ?"]
-        random_text = apply_options(random_strings, user_option)
+        # Detection and translation pipeline for Microsoft, Google APIs
+        if detection_method in ['google', 'google_advanced', 'microsoft', 'microsoft_advanced']:
+            advanced_mode = True if 'advanced' in detection_method else False
+            image_path = os.path.join(UPLOAD_FOLDER, 'results', filename)
+            if 'google' in detection_method:
+                overall_text, words, words_and_boxes = g_analyze(image_path, advanced_mode)
+            elif 'microsoft' in detection_method:
+                overall_text, words, words_and_boxes = ms_analyze(image_path, advanced_mode)
 
-        return render_template('analysed.html', filename=filename, random_text=random_text, image=image)
+            if all([overall_text, words, words_and_boxes]):
+                translated_text = apply_options([overall_text], user_option)[0].pop()
+                translated_words, det_lang = apply_options(words, user_option)
+                from_to_words_dict = {words[i]: translated_words[i] for i in range(0, len(words))}
+                translated_words_and_boxes = [[translated_words[i], word_box[1]] for i, word_box in enumerate(words_and_boxes)]
+                if 'google' in detection_method:
+                    g_draw_results(translated_words_and_boxes, None, image_path, filename, 'translated')
+                elif 'microsoft' in detection_method:
+                    ms_draw_results(translated_words_and_boxes, None, image_path, filename, 'translated', advanced_mode)
+
+                overall_text = overall_text.replace('\n', '<br />')
+                translated_text = translated_text.replace('\n', '<br />')
+
+                return render_template('results.html',
+                                       detected_language=det_lang,
+                                       to_language=to_language,
+                                       original_text=overall_text,
+                                       translated_text=translated_text,
+                                       from_to_words=from_to_words_dict,
+                                       original_filename=filename,
+                                       partial_filename='granular_partial_' + filename,
+                                       translated_filename='granular_translated_' + filename)
+
+            else:
+                return render_template('results.html', error='There was no text detected or the image was not accepted by the API!')
     else:
-        return render_template('analysed.html', error='Image not uploaded!')
+        return render_template('results.html', error='Image not uploaded!')
 
 
 def option_dumps(options_index, translate_to):
-    selected_option = possible_options.get(options_index)
+    selected_option = possible_lang_options.get(options_index)
     selected_option['translate_to'] = get_country_language(translate_to)
     return json.dumps(selected_option)
 
