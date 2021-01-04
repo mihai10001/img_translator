0a32408d07938c60c2dd139df7ade11bbb848892
mihai10001 <rvivits@gmail.com>
2021-01-04 21:32:21 +0200

    add dictionary containing detection methods available

---
+++ app/detect/detection_methods.py
@@ -0,0 +1,14 @@
+possible_detection_methods = {
+    # 0: Use the Microsoft Computer Vision API
+    '0': 'microsoft',
+    # 1: Use the Microsoft Computer Vision advanced API
+    '1': 'microsoft_advanced',
+    # 2: Use the Google Vision API
+    '2': 'google',
+    # 3: Use the Google Vision advanced API
+    '3': 'google_advanced',
+    # 4: Use the Abby API
+    '4': 'abbyy',
+    # 5: Use the Tesseract SDK
+    '5': 'tesseract',
+}
