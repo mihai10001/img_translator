2726e6b469d8d0adfc8af84c7b68b0e221832d49
mihai10001 <rvivits@gmail.com>
2021-01-04 21:44:06 +0200

    temp. fix: use unofficial package untill py-googletrans gets updated

--- app/language/translate.py
+++ app/language/translate.py
@@ -5,7 +5,7 @@ def g_translate(string, to_language, from_language=None):
     # Translate the text using googletrans
     # It is a library that implements the Google Translate API
     # Auto generates a 'ticket' in order to make API calls
-    translator = Translator()
+    translator = Translator(service_urls=['translate.googleapis.com'])
 
     # Used togheter with language_detect.py
     # Will use it's own autodetect if from_language is not provided
@@ -18,7 +18,7 @@ def g_translate(string, to_language, from_language=None):
 def g_bulk_translate(string_list, to_language, from_language=None):
     # Similar to the above, but will bulk translate an array of strings ('texts')
     # Advantage: one bigger API call than a lot of smaller ones
-    translator = Translator()
+    translator = Translator(service_urls=['translate.googleapis.com'])
 
     if from_language:
         translations = translator.translate(string_list, src=from_language, dest=to_language)
